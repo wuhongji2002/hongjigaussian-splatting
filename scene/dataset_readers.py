3afe460eaaf9b349e33a88c9d44b4e200fcecacb
wuhongji <2444073412@qq.com>
2026-05-22 10:02:17 +0800

    修改了dataset_readers.py 第 98 行左右的检测逻辑对SIMPLE_RADIAL的豁免放行intr.model=="SIMPLE_RADIAL"

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -94,8 +94,12 @@ def readColmapCameras(cam_extrinsics, cam_intrinsics, depths_params, images_fold
             focal_length_y = intr.params[1]
             FovY = focal2fov(focal_length_y, height)
             FovX = focal2fov(focal_length_x, width)
+        elif intr.model=="SIMPLE_RADIAL" or intr.model=="RADIAL":
+            focal_length_x = intr.params[0]
+            FovY = focal2fov(focal_length_x, height)
+            FovX = focal2fov(focal_length_x, width)
         else:
-            assert False, "Colmap camera model not handled: only undistorted datasets (PINHOLE or SIMPLE_PINHOLE cameras) supported!"
+            assert False, f"Colmap camera model {intr.model} not handled: only PINHOLE, SIMPLE_PINHOLE, SIMPLE_RADIAL, RADIAL supported!"
 
         n_remove = len(extr.name.split('.')[-1]) + 1
         depth_params = None
